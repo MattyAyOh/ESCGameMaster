1fe8a6ebf82175fbedf29430ac1f37a678bc5153
Matt Ao <=>
2018-04-23 06:12:43 -0400

    custom cell with text view

---
+++ GameMaster/ESCTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ESCTableViewCell.swift
+//  GameMaster
+//
+//  Created by Matt Ao on 4/23/18.
+//  Copyright © 2018 Matt Ao. All rights reserved.
+//
+
+import UIKit
+
+class ESCTableViewCell:UITableViewCell {
+    @IBOutlet weak var textView: UITextView!
+}
