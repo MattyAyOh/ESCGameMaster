e2c4a61e81854eb01b3d3f4cf35bc8034c455af3
Matt Ao <=>
2018-04-23 04:20:35 +0300

    round the rectangles

--- GameMaster/ViewController.swift
+++ GameMaster/ViewController.swift
@@ -20,20 +20,29 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     let publicDB = CKContainer.init(identifier: "iCloud.esc.GameMaster").publicCloudDatabase
     
-    @IBOutlet weak var questionTextView: UITextView!
-    @IBOutlet weak var hintTableView: UITableView!
-    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-    
+    @IBOutlet weak var questionsTableView: UITableView!
+    @IBOutlet weak var precannedHintsTableView: UITableView!
+    @IBOutlet weak var hintTextView: UITextView!
+    @IBOutlet weak var previousHintsTableView: UITableView!
+    @IBOutlet weak var questionsActivityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var precannedActivityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var previousActivityIndicator: UIActivityIndicatorView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//        fetchAllHints()
+        let tables = [questionsTableView, precannedHintsTableView, previousHintsTableView]
+        for table in tables {
+            table?.tableFooterView = UIView()
+            table?.layer.cornerRadius = 10
+            table?.layer.masksToBounds = true
+        }
+
+        
+        hintTextView.layer.cornerRadius = 10
+        hintTextView.layer.masksToBounds = true
+        hintTextView.textContainerInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
